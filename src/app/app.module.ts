fcadcf48b057a7a700a84378f3892bcd04dda843
Elad Bezalel <elad.bezalel@gmail.com>
2017-06-20 15:36:07 +0300

    initial upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
+import {
+  MdButtonModule,
+  MdCardModule,
+  MdExpansionModule,
+  MdToolbarModule
+} from '@angular/material';
 
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+import {AppComponent} from './app.component';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    FlexLayoutModule,
+
+    MdCardModule,
+    MdToolbarModule,
+    MdButtonModule,
+    MdExpansionModule
   ],
-  providers: [],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
